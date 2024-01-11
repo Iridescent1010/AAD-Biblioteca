5e13ef91ac8118d9293275afc919db9b0265f5b2
Red One <saxovtsfactory16@gmail.com>
2024-01-11 09:49:59 +0100

    Implementación DAO para Hibernate

---
+++ Biblioteca MVC/src/modelo/dao/CategoriaDAOHibernate.java
@@ -0,0 +1,32 @@
+package modelo.dao;
+
+import modelo.Categoria;
+
+import java.util.List;
+
+public class CategoriaDAOHibernate implements CategoriaDAO {
+    @Override
+    public boolean inserta(Categoria categoria) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean modificar(Categoria categoria) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean borrar(int id) throws Exception {
+        return false;
+    }
+
+    @Override
+    public Categoria categoria(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Categoria> leerAllCategorias() throws Exception {
+        return null;
+    }
+}
---
+++ Biblioteca MVC/src/modelo/dao/PrestamoDAOHibernate.java
@@ -0,0 +1,32 @@
+package modelo.dao;
+
+import modelo.Prestamo;
+
+import java.util.List;
+
+public class PrestamoDAOHibernate implements PrestamoDAO{
+    @Override
+    public boolean insertar(Prestamo prestamo) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean modificar(Prestamo prestamo) throws Exception {
+        return false;
+    }
+
+    @Override
+    public boolean borrar(int id) throws Exception {
+        return false;
+    }
+
+    @Override
+    public List<Prestamo> leerAllPrestamos() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Prestamo getPrestamo(int id) throws Exception {
+        return null;
+    }
+}
